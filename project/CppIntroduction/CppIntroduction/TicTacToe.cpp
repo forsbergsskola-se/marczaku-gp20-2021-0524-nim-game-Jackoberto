6347043c06e4f73d2f71436f76a12313486f47d7
Jackoberto <jackoberto01@gmail.com>
2021-05-25 11:37:02 +0200

    Adds win condition which works

--- project/CppIntroduction/CppIntroduction/TicTacToe.cpp
+++ project/CppIntroduction/CppIntroduction/TicTacToe.cpp
@@ -44,19 +44,57 @@ void TicTacToe::getPlayerInput(bool players_turn)
 
 int TicTacToe::checkWinCondition()
 {
-    return 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] != board[i][1] || board[i][1] != board[i][2]) 
+            continue;
+        if (board[i][0] != 0)
+            return board[i][ 0];
+    }
+        
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[0][i] != board[1][i] || board[1][i] != board[2][i]) 
+            continue;
+        if (board[0][i] != 0)
+            return board[i][0];
+    }
+        
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) 
+        if (board[1][1] != 0)
+            return board[1][1];
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) 
+        if (board[1][1] != 0)
+            return board[1][1];
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[i][j] == 0)
+                return 0;
+        }
+    }
+        
+    return -1;
 }
 
 void TicTacToe::ticTacToeTwoPlayer()
 {
-    bool players_turn = (rand() + 1) % 2 == 0;
+    bool players_turn = false;
     displayBoard();
-    while (checkWinCondition() == 0)
+    int winner = 0;
+    while (winner == 0)
     {
         players_turn = !players_turn;
+        cout << "Player " << (players_turn ? 1 : 2) << endl;
         getPlayerInput(players_turn);
         displayBoard();
+        winner = checkWinCondition();
     }
+    if (winner == -1)
+        cout << "It's a draw";
+    cout << "Winner is Player " << winner;
 }
 
 void TicTacToe::ticTacToeAI()
