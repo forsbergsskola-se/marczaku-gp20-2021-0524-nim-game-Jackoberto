efd1c38c0e093c3c0d2ffbd4fef61a9b34f1417e
Jackoberto <jackoberto01@gmail.com>
2021-05-26 12:56:05 +0200

    Adds with binary as well

--- project/CppIntroduction/CppIntroduction/BitConverter.cpp
+++ project/CppIntroduction/CppIntroduction/BitConverter.cpp
@@ -29,23 +29,14 @@ void BitConverter::decimalToBinary(unsigned n)
 void BitConverter::decimalToBinaryExtra(unsigned n)
 {
     const int number_of_bits = 32;
-    int binaryNum[number_of_bits];
- 
-    unsigned i = 0;
-    while (n > 0) {
- 
-        binaryNum[i] = n % 2;
-        n = n >> 1;
-        i++;
-    }
-    for (; i < number_of_bits; i++)
-    {
-        binaryNum[i] = 0;
-    }
-
     cout << "0b";
-    for (int j = number_of_bits - 1; j >= 0; j--)
-        cout << binaryNum[j];
+    for (int i = number_of_bits - 1; i >= 0; i--) {
+        int k = n >> i;
+        if (k & 1)
+            cout << "1";
+        else
+            cout << "0";
+    }
 }
 
 unsigned BitConverter::getNumber()
