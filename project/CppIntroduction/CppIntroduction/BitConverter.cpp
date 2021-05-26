c2e8326a9060078f011ca0d6ed1fbc83f33965b7
Jackoberto <jackoberto01@gmail.com>
2021-05-26 12:46:20 +0200

    Adds a simple bitconverter

---
+++ project/CppIntroduction/CppIntroduction/BitConverter.cpp
@@ -0,0 +1,69 @@
+#include "BitConverter.h"
+#include <iostream>
+#include "ConsoleFunctions.h"
+
+using namespace std;
+
+void BitConverter::decimalToBinary(unsigned n)
+{
+    const int number_of_bits = 32;
+    int binaryNum[number_of_bits];
+ 
+    unsigned i = 0;
+    while (n > 0) {
+ 
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    for (; i < number_of_bits; i++)
+    {
+        binaryNum[i] = 0;
+    }
+
+    cout << "0b";
+    for (int j = number_of_bits - 1; j >= 0; j--)
+        cout << binaryNum[j];
+}
+
+void BitConverter::decimalToBinaryExtra(unsigned n)
+{
+    const int number_of_bits = 32;
+    int binaryNum[number_of_bits];
+ 
+    unsigned i = 0;
+    while (n > 0) {
+ 
+        binaryNum[i] = n % 2;
+        n = n >> 1;
+        i++;
+    }
+    for (; i < number_of_bits; i++)
+    {
+        binaryNum[i] = 0;
+    }
+
+    cout << "0b";
+    for (int j = number_of_bits - 1; j >= 0; j--)
+        cout << binaryNum[j];
+}
+
+unsigned BitConverter::getNumber()
+{
+    unsigned i = 0;
+    while (true)
+    {
+        cout << "Write A Positive Integer" << endl;
+        cin >> i;
+        CHECK_CONSOLE_ERROR()
+        else
+            return i;
+    }
+}
+
+int BitConverter::run()
+{
+    auto n = getNumber();
+    decimalToBinaryExtra(n);
+    return 0;
+}
--- project/CppIntroduction/CppIntroduction/CppIntroduction.cpp
+++ project/CppIntroduction/CppIntroduction/CppIntroduction.cpp
@@ -1,9 +1,11 @@
+#include "BitConverter.h"
 #include "Nim.h"
 #include "TicTacToe.h"
 
 int main(int argc, char* argv[])
 {
     auto nim = Nim(24);
-    auto ticTacToe = TicTacToe();
-    return ticTacToe.run();
+    auto tic_tac_toe = TicTacToe();
+    auto bit_converter = BitConverter();
+    return bit_converter.run();
 }
