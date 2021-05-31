a76e1cc967cba237be0d929c71a203cd8804822c
Jackoberto <jackoberto01@gmail.com>
2021-05-31 15:56:31 +0200

    Adds Fibonacci both recursively and iteratively

--- project/CppIntroduction/CppIntroduction/CppIntroduction.cpp
+++ project/CppIntroduction/CppIntroduction/CppIntroduction.cpp
@@ -4,13 +4,15 @@
 #include "Nim.h"
 #include "CodingChallenges.h"
 #include "TicTacToe.h"
+#include "Fibonacci.h"
 
 int main(int argc, char* argv[])
 {
     // std::cout << "Hello Ends With ello " << CodingChallenges::endsWith("Hello", "ello") << std::endl;
     // std::cout << "Digital Root Of 15 Is " << CodingChallenges::digitalRoot(156);
-    auto nim = Nim(24);
-    auto tic_tac_toe = TicTacToe();
-    auto bit_converter = BitConverter();
-    return tic_tac_toe.run();
+    // auto nim = Nim(24);
+    // auto tic_tac_toe = TicTacToe();
+    // auto bit_converter = BitConverter();
+    // return tic_tac_toe.run();
+    Fibonacci::test(10);
 }
---
+++ project/CppIntroduction/CppIntroduction/Fibonacci.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+namespace Fibonacci
+{
+    inline int iteratively(int times_to_run = 5)
+    {
+        int y = 0, x = 1, z = 0;
+        for (int i = 0; i < times_to_run; i++)
+        {
+            z = x + y;
+            int even = i%2;
+            if (even == 0)
+            {
+                x = z;
+            }
+            else
+                y = z;
+        }
+        return z;
+    }
+
+    inline int recursively(int n = 5)
+    {
+        if(n==1||n==0){
+            return n;
+        }
+        return recursively(n-1)+recursively(n-2);
+    }
+
+    inline void test(int n)
+    {
+        cout << "Getting " << n << "th Fibonacci number" << endl; 
+        auto start = chrono::high_resolution_clock::now();
+        cout << recursively(10) << endl;
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+        cout << "Recursive Fibonacci Took " << duration.count() << " microseconds" << endl;
+
+        start = chrono::high_resolution_clock::now();
+        cout << iteratively(10) << endl;
+        stop = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+        cout << "Iterative Fibonacci Took " << duration.count() << " microseconds" << endl;
+    }
+}
