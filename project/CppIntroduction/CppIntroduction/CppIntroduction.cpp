b058b7f031c922f861a3b451d7db2a9d9a56c43f
Jackoberto <jackoberto01@gmail.com>
2021-05-31 16:09:31 +0200

    moves Fibonacci code to new battleship repo

--- project/CppIntroduction/CppIntroduction/CppIntroduction.cpp
+++ project/CppIntroduction/CppIntroduction/CppIntroduction.cpp
@@ -4,15 +4,13 @@
 #include "Nim.h"
 #include "CodingChallenges.h"
 #include "TicTacToe.h"
-#include "Fibonacci.h"
 
 int main(int argc, char* argv[])
 {
     // std::cout << "Hello Ends With ello " << CodingChallenges::endsWith("Hello", "ello") << std::endl;
     // std::cout << "Digital Root Of 15 Is " << CodingChallenges::digitalRoot(156);
-    // auto nim = Nim(24);
-    // auto tic_tac_toe = TicTacToe();
-    // auto bit_converter = BitConverter();
-    // return tic_tac_toe.run();
-    Fibonacci::test(30);
+    auto nim = Nim(24);
+    auto tic_tac_toe = TicTacToe();
+    auto bit_converter = BitConverter();
+    return tic_tac_toe.run();
 }
